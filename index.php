af2bfb2599c1492c9b59eef317dad4f34959e27a
TheKevinPeltier <k-peltier@outlook.com>
2025-07-28 19:07:37 -0400

    Layout and thought

--- index.php
+++ index.php
@@ -3,16 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Quotes</title>
+    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
 </head>
-<body>
+<body class="background-image">
 
-    <div class="bg-primary">
-        Test
+    <div class="w-100 d-flex justify-content-center align-content-center vh-100">
+        <div class="w-50 d-flex flex-column mx-auto justify-content-center">
+            <h1 class="w-50 mx-auto text-center text-white text-shadow">Read a great quote!</h1>
+            <div class=""></div>
+            <button type="button" class="btn btn-primary w-25 mx-auto shadow-lg" data-bs-toggle="modal" data-bs-target="#quoteModal">CLICK!!!!!</button>
+        </div>
     </div>
 
-<script src="/node_modules/bootstrap/dist/js/bootstrap.js"></script>
+
+
+
+    <div class="modal fade" id="quoteModal" tabindex="-1" aria-labelledby="exampleModalLabel">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Marcus Aurelius - Meditations</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    You have power over your mind - not outside events. Realize this, and you will find strength.
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.js"></script>
 
 </body>
 </html>
